4815cd6eb028b05d3f9a3f7063e4963a1f4ce6d0
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-01 18:05:01 +0200

    header add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, {useState,useEfect} from 'react';
+import React, {useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
 
+const [items,setItems]=useState([]);
+const [visible,setVisible]=useState(3);
+
+
+useEffect(()=>{
+  fetch("https://themealdb.com/api/json/v1/1/filter.php?a=british")
+  .then((res)=>res.json())
+  .then((data)=> setItems(data.meals))
+},[])
 
 
   return (
     <div className="App">
-     
+     <h1 className='header'>British Meals</h1>
     </div>
   );
 }

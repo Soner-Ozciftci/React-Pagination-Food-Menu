bee7a7efa563e051a64aeeb0c9feb84de680fe03
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-01 18:16:41 +0200

    img

--- src/App.js
+++ src/App.js
@@ -16,8 +16,19 @@ useEffect(()=>{
 
   return (
     <div className="App">
-     <h1 className='header'>British Meals</h1>
-    </div>
+     <h1 className='header'>British Meals <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Flag_of_the_United_Kingdom_%283-5%29.svg/640px-Flag_of_the_United_Kingdom_%283-5%29.svg.png'/h1>
+   <div className='container'>
+  {items.map(item => (
+       <div className='card'>
+        <div className='image'>
+      <img src={item.strMealThumb} />
+        </div>
+        <p>{item.strMeal}</p>
+       </div>
+))}
+
+   </div>
+  </div>
   );
 }
 

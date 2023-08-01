d5c4d2e24c270d2a627b0381ccf5970e658874d8
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-01 18:48:52 +0200

    bristish img menu

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import React, {useState,useEffect} from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [visible, setVisible] = useState(3);
 
-const [items,setItems]=useState([]);
-const [visible,setVisible]=useState(3);
+  function showMoreFood() {
+    setVisible((item) => (item += 3));
+  }
 
-
-useEffect(()=>{
-  fetch("https://themealdb.com/api/json/v1/1/filter.php?a=british")
-  .then((res)=>res.json())
-  .then((data)=> setItems(data.meals))
-},[])
-
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?a=british")
+      .then((res) => res.json())
+      .then((data) => setItems(data.meals));
+  }, []);
 
   return (
     <div className="App">
-     <h1 className='header'>British Meals <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Flag_of_the_United_Kingdom_%283-5%29.svg/640px-Flag_of_the_United_Kingdom_%283-5%29.svg.png'/h1>
-   <div className='container'>
-  {items.map(item => (
-       <div className='card'>
-        <div className='image'>
-      <img src={item.strMealThumb} />
-        </div>
-        <p>{item.strMeal}</p>
-       </div>
-))}
-
-   </div>
-  </div>
+      <h1 className="header">
+        British Meals{" "}
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Great_Britain_%281707%E2%80%931800%29.svg/2560px-Flag_of_Great_Britain_%281707%E2%80%931800%29.svg.png" />
+      </h1>
+      <div className="container">
+        {items.slice(0, visible).map((item) => (
+          <div className="card">
+            <div className="image">
+              <img src={item.strMealThumb} />
+            </div>
+            <p>{item.strMeal}</p>
+          </div>
+        ))}
+        <button onClick={showMoreFood}>Click for More Food</button>
+      </div>
+    </div>
   );
 }
 
